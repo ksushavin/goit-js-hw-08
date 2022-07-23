5a517e1c5f36bad5dedfedbdf7d4f4fb2fa2b758
Oksana Bulakh <ksushavin15@gmail.com>
2022-07-23 20:32:07 +0200

    додає js до 2 задачі

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(saveVideoplayerCurrentTime, 1000));
+
+function saveVideoplayerCurrentTime({ seconds }) {
+    localStorage.setItem("videoplayer-current-time", seconds) 
+}
+
+const videoplayerSavedTime = localStorage.getItem("videoplayer-current-time");
+
+// Відтворення відео зі збереженної позиції
+player.setCurrentTime(videoplayerSavedTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
 
